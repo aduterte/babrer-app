53b076a98b97b94ab863f52f7a72f91b6be0b9bf
aduterte <alexduterte@gmail.com>
2021-03-15 20:58:18 -0400

    jwt auth added

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,14 @@
 class ApplicationController < ActionController::API
+
+    def secret_key
+        "barBque"
+    end
+
+    def encode(payload)
+        JWT.encode(payload, secrete_key, 'HS256')
+    end
+
+    def decode(token)
+        JWT.decode(token, "barBque", true, {algorithm: 'HS256'})[0]
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,8 @@ Rails.application.routes.draw do
   resources :clients
   resources :barbers
   resources :appointments
+  
+  post "/logins", to: "logins#login"
+  get "/logins", to: "logins#token_authenticate"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
